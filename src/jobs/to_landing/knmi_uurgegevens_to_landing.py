a0339d5a2eeb9bedd51a20ed0e9b3d132d0aff20
jesse <jesse@pop-os.localdomain>
2021-08-24 23:22:27 +0200

    moved folders

--- src/jobs/to_landing/knmi_uurgegevens_to_landing.py
+++
@@ -1,7 +0,0 @@
-from datetime import date, timedelta
-from knmi import knmi
-
-
-# TODO fetch the last run date
-
-knmi.load_knmi_to_landing('https://www.daggegevens.knmi.nl/klimatologie/uurgegevens', 'uurgegevens', date.today() - timedelta(days=1))
