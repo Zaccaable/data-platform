241e66af8a5d0aac2819713f89813deeb834ca34
Zaccaable <jpieters23@hotmail.com>
2022-12-17 14:42:25 +0100

    moved jobs

--- jobs/python/src/to_landing/knmi_daggegevens_to_landing.py
+++
@@ -1,7 +0,0 @@
-from datetime import date, timedelta
-from knmi import knmi
-
-
-# TODO fetch the last run date
-
-knmi.load_knmi_to_landing('https://www.daggegevens.knmi.nl/klimatologie/daggegevens', 'daggegevens', date.today() - timedelta(days=1))
